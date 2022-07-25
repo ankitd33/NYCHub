1e1a3be24713b8b76a6cf75ff3ed7ed7fd8e779f
Ankit Deshmukh <ankitd@fb.com>
2022-07-25 00:39:06 -0400

    Adding material UI and some basic components

--- nyc-hub-react/src/App.js
+++ nyc-hub-react/src/App.js
@@ -1,5 +1,13 @@
-import React, {useEffect } from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import RestoreIcon from '@mui/icons-material/Restore';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
 function App() {
   useEffect(() => {
@@ -98,15 +106,34 @@ function App() {
     .catch(error => console.log(error));
   }, []);
 
+  const [value, setValue] = useState("Recents");
+
 
 
   return (
     <div className="App">
+      {/* This header className is the styling to make up the header take the whole page -  check css*/}
       <header className="App-header">
         <p>
           Hello
         </p>
       </header>
+      <Fab color="primary" aria-label="add">
+  <AddIcon />
+</Fab>
+      <Box sx={{ width: 500 }}>
+      <BottomNavigation
+    showLabels
+    value={value}
+    onChange={(event, newValue) => {
+      setValue(newValue);
+    }}
+  >
+    <BottomNavigationAction label="Recents" icon={<RestoreIcon />} value="Recents"/>
+    <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} value="Favorites"/>
+    <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} value="Nearby"/>
+  </BottomNavigation>
+  </Box>
     </div>
   );
 }
